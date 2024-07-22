aa43a6bfd332674c8026ebc3c86a1490de664ace
Mitchell Youssef <129881959+shoosTGIT@users.noreply.github.com>
2024-07-22 17:16:42 -0400

    Pushing Files

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -6,6 +6,15 @@ namespace Framework;
 
 class App
 {
+    private Router $router;
+    //private $test;
+
+    public function __construct()
+    {
+        $this->router = new Router();
+        //$this->test = $Test;
+    }
+
     public function run()
     {
         echo "Application is running";
