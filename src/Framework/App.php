615ff972d2d4e009549cfe5f92fef0b8398569a3
Mitchell Youssef <129881959+shoosTGIT@users.noreply.github.com>
2024-07-25 13:53:38 -0400

    Added HTTP Methods within Routes

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -7,16 +7,19 @@ namespace Framework;
 class App
 {
     private Router $router;
-    //private $test;
 
     public function __construct()
     {
         $this->router = new Router();
-        //$this->test = $Test;
     }
 
     public function run()
     {
         echo "Application is running";
+    }
+
+    public function get(string $path)
+    {
+        $this->router->add('GET', $path);
     }
 }
