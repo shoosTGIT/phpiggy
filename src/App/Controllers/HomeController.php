14c378409465116440ae4d5a0e501532fe9b4749
Mitchell Youssef <129881959+shoosTGIT@users.noreply.github.com>
2024-07-29 15:07:12 -0400

    Created and incorporated a basic template engine

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -4,10 +4,20 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Framework\TemplateEngine;
+
 class HomeController
 {
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine();
+    }
+
     public function home()
     {
+        dd($this->view);
         echo 'home page';
     }
 }
