431b17f0ec1585ec2199de3f861c3b456981552e
Mitchell Youssef <129881959+shoosTGIT@users.noreply.github.com>
2024-08-06 01:48:42 -0400

    Validated and Registered a Rule

--- src/App/Services/ValidatorService.php
+++ src/App/Services/ValidatorService.php
@@ -4,6 +4,22 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use Framework\Validator;
+use Framework\Rules\{RequiredRule};
+
 class ValidatorService
 {
+    private Validator $validator;
+
+    public function __construct()
+    {
+        $this->validator = new Validator();
+
+        $this->validator->add('required', new RequiredRule());
+    }
+
+    public function validateRegister(array $formData)
+    {
+        $this->validator->validate($formData);
+    }
 }
