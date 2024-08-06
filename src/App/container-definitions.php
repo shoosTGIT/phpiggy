09e68c39f4d9c603ccd1b8207dbf341a5fa58182
Mitchell Youssef <129881959+shoosTGIT@users.noreply.github.com>
2024-08-06 01:28:08 -0400

    Injected new Validator Service

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 use Framework\TemplateEngine;
 use App\Config\Paths;
+use App\Services\ValidatorService;
 
 
 return [
-    TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW)
+    TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW),
+    ValidatorService::class => fn () => new ValidatorService()
 ];
