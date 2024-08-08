8eac0c04ca6625f58c1e2c0b799dddeb65114f66
Mitchell Youssef <129881959+shoosTGIT@users.noreply.github.com>
2024-08-08 15:41:30 -0400

    Added more rules

--- src/App/Services/ValidatorService.php
+++ src/App/Services/ValidatorService.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 namespace App\Services;
 
 use Framework\Validator;
-use Framework\Rules\{RequiredRule, EmailRule, MinRule};
+use Framework\Rules\{
+    RequiredRule,
+    EmailRule,
+    MinRule,
+    InRule,
+    UrlRule,
+    MatchRule
+};
 
 class ValidatorService
 {
@@ -18,6 +25,9 @@ class ValidatorService
         $this->validator->add('required', new RequiredRule());
         $this->validator->add('email', new EmailRule());
         $this->validator->add('min', new MinRule());
+        $this->validator->add('in', new InRule());
+        $this->validator->add('url', new UrlRule());
+        $this->validator->add('match', new MatchRule());
     }
 
     public function validateRegister(array $formData)
@@ -25,10 +35,10 @@ class ValidatorService
         $this->validator->validate($formData, [
             'email' => ['required', 'email'],
             'age' => ['required', 'min:18'],
-            'country' => ['required'],
-            'socialMediaURL' => ['required'],
+            'country' => ['required', 'in:USA,Canada,Mexico'],
+            'socialMediaURL' => ['required', 'url'],
             'password' => ['required'],
-            'confirmPassword' => ['required'],
+            'confirmPassword' => ['required', 'match:password'],
             'tos' => ['required']
         ]);
     }
