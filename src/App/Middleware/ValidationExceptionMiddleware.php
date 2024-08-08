87a596816d324cbca6d61f864a5a1f730c8b50ab
Mitchell Youssef <129881959+shoosTGIT@users.noreply.github.com>
2024-08-08 16:09:35 -0400

    Excluded sensitive information from being accessed

--- src/App/Middleware/ValidationExceptionMiddleware.php
+++ src/App/Middleware/ValidationExceptionMiddleware.php
@@ -14,8 +14,16 @@ class ValidationExceptionMiddleware implements MiddlewareInterface
         try {
             $next();
         } catch (ValidationException $e) {
+            $oldFormData = $_POST;
+
+            $excludedFields = ['password', 'confirmPassword'];
+            $formattedFormData = array_diff_key(
+                $oldFormData,
+                array_flip($excludedFields)
+            );
+
             $_SESSION['errors'] = $e->errors;
-            $_SESSION['oldFormData'] = $_POST;
+            $_SESSION['oldFormData'] = $formattedFormData;
 
             $referer = $_SERVER['HTTP_REFERER'];
             redirectTo($referer);
