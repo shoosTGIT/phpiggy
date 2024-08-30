505b3261095ee657bc853f8b936e9c1104ae38f2
Mitchell Youssef <129881959+shoosTGIT@users.noreply.github.com>
2024-08-30 15:14:04 -0400

    Added a Command line injection

---
+++ cli.php
@@ -0,0 +1,20 @@
+<?php
+
+$driver = 'mysql';
+
+$config = http_build_query(data: [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'phpiggy'
+], arg_separator: ';');
+
+$dsn = "{$driver}:{$config}";
+$username = 'root';
+$password = '';
+
+try {
+    $db = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+    die("Unable to connect to database");
+}
+echo "Connected to database";
