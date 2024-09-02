a5a056cc8ac763aabac07200808f5c5e379e2e9a
Mitchell Youssef <129881959+shoosTGIT@users.noreply.github.com>
2024-09-02 15:34:33 -0400

    Added a table and loaded files into CLI

--- cli.php
+++ cli.php
@@ -10,30 +10,6 @@ $db = new Database('mysql', [
     'dbname' => 'phpiggy'
 ], 'root', '');
 
-try {
-    $db->connection->beginTransaction();
+$sqlFile = file_get_contents("./database.sql");
 
-    $db->connection->query("INSERT INTO products VALUES(99,'Gloves')");
-
-    $search = "Hats";
-    $query = "SELECT * FROM products WHERE name=:name";
-
-    $stmt = $db->connection->prepare($query);
-
-    $stmt->bindValue('name', 'Gloves', PDO::PARAM_STR);
-
-    $stmt->execute([
-        'name' => $search
-    ]);
-
-    var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
-
-    $db->connection->commit();
-} catch (Exception $error) {
-    if ($db->connection->inTransaction()) {
-        $db->connection->rollBack();
-    }
-
-
-    echo "Transaction Failed!";
-}
+$db->connection->query($sqlFile);
