d20f3c806ba5870cfe35dc1b6d531e4b1483e18e
Mitchell Youssef <129881959+shoosTGIT@users.noreply.github.com>
2024-09-02 15:04:54 -0400

    Set up Prepared Statements

--- cli.php
+++ cli.php
@@ -10,4 +10,30 @@ $db = new Database('mysql', [
     'dbname' => 'phpiggy'
 ], 'root', '');
 
-echo "Connected to database";
+try {
+    $db->connection->beginTransaction();
+
+    $db->connection->query("INSERT INTO products VALUES(99,'Gloves')");
+
+    $search = "Hats";
+    $query = "SELECT * FROM products WHERE name=:name";
+
+    $stmt = $db->connection->prepare($query);
+
+    $stmt->bindValue('name', 'Gloves', PDO::PARAM_STR);
+
+    $stmt->execute([
+        'name' => $search
+    ]);
+
+    var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
+
+    $db->connection->commit();
+} catch (Exception $error) {
+    if ($db->connection->inTransaction()) {
+        $db->connection->rollBack();
+    }
+
+
+    echo "Transaction Failed!";
+}
