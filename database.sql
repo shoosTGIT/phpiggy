7a751fec8bdb74b98c3d3319d69cbc2e169e17d1
Mitchell Youssef <129881959+shoosTGIT@users.noreply.github.com>
2024-09-13 16:19:00 -0400

    Refactored the Query

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users(
+CREATE TABLE IF NOT EXISTS users(
   id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   email varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
@@ -7,6 +7,6 @@ CREATE TABLE users(
   social_media_url varchar(255) NOT NULL,
   created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY(id)
+  PRIMARY KEY(id),
   UNIQUE KEY(email)
 );
